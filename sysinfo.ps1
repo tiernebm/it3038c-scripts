cbb1d206d89a9ef72849611b9346f33bff1e0170
tiernebm <tiernebm@mail.uc.edu>
2019-10-03 19:55:18 -0400

    adding powershell files

---
+++ sysinfo.ps1
@@ -0,0 +1,9 @@
+﻿$Date1 = Get-Date
+function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"
+}
+$IP = getIP
+Write-Host("$IP")
+$Body = "This Machines IP is $IP. User is $env:Username. Hostname is $env:ComputerName. Powershell Version 5. Today's date is $date1"
+
+Send-MailMessage -To "bmtierney72@gmail.com" -from "bmtierney72@gmail.com" -Subject "IT3038C Windows Sysinfo" -Body $Body -SMTPServer smtp.gmail.com -Port 587 -useSSL -Credential (Get-Credential)
