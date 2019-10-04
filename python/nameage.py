5516be90310ba8df812c080880e3b68b0100e3a1
tiernebm <56139559+tiernebm@users.noreply.github.com>
2019-10-03 21:24:43 -0400

    Update nameage.py

--- python/nameage.py
+++ python/nameage.py
@@ -1,1 +1,6 @@
+print('What is your name?')
+myName = input()
+print('Hello, ' + myName + '. That is a good name. How old are you?')
+myAge = input()
+print(myAge+' ? That's funny, I'm only a few seconds old.')
 
